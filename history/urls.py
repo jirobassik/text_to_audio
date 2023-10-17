fd0f892f8dccac0ad8ab427f02077fa428dfe317
= <jirobassik@tut.by>
2023-10-17 21:55:15 +0300

    Add choose search

--- history/urls.py
+++ history/urls.py
@@ -4,4 +4,5 @@ from history.views import HistoryView, HistoryAiSearchView, HistorySearchView
 urlpatterns = [
     path('', HistoryView.as_view(), name='history-view'),
     path('search', HistorySearchView.as_view(), name='history-search'),
+    path('search-ai', HistoryAiSearchView.as_view(), name='history-ai-search'),
 ]
