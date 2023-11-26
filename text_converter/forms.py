36e9692999fdcd54f01a72fba4ec978df21250ef
= <jirobassik@tut.by>
2023-11-26 12:16:02 +0300

    Update text_to_audio forms

--- text_converter/forms.py
+++ text_converter/forms.py
@@ -2,28 +2,6 @@ from django import forms
 from operator import getitem
 
 
-class TextConverterLoginForm(forms.Form):
-    text = forms.CharField(max_length=400, min_length=1, required=True, widget=forms.Textarea, initial='Привет мир',
-                           label='Текст')
-    voice = forms.ChoiceField(choices=[], widget=forms.Select(attrs={'id': 'voice-select'}), label='Голос')
-    preset = forms.ChoiceField(choices=[('ultra_fast', 'Очень быстро'), ('fast', 'Быстро'),
-                                        ('quality', 'Качество'), ('high_quality', 'Высокое качество')],
-                               label='Скорость/Качество')
-
-    def __init__(self, *args, **kwargs):
-        votes = kwargs.pop('votes', [])
-        user_votes = kwargs.pop('user_votes', [])
-        super(TextConverterLoginForm, self).__init__(*args, **kwargs)
-        self.fields['voice'].choices = self.get_choices(votes, user_votes)
-
-    @staticmethod
-    def get_choices(votes, user_votes):
-        voice_choice = (('Стандартные голоса', [(vote.get('id'), vote.get('audio_name')) for vote in votes]),
-                        ('Пользовательские голоса', [(vote.get('id'), vote.get('audio_name')) for vote in user_votes]))
-        choices = [choice for choice in voice_choice if getitem(choice, 1)]
-        return choices
-
-
 class TextConverterForm(forms.Form):
     text = forms.CharField(max_length=400, min_length=1, required=True, widget=forms.Textarea, initial='Привет мир',
                            label='Текст')
@@ -34,11 +12,24 @@ class TextConverterForm(forms.Form):
                                label='Скорость/Качество')
 
     def __init__(self, *args, **kwargs):
-        votes = kwargs.pop('votes', [])
+        self.votes = kwargs.pop('votes', [])
+        self.user_votes = kwargs.pop('user_votes', [])
         super(TextConverterForm, self).__init__(*args, **kwargs)
-        self.fields['voice'].choices = self.get_choices(votes)
+        self.fields['voice'].choices = self.get_choices()
 
-    @staticmethod
-    def get_choices(votes):
-        choices = [('Стандартные голоса', [(vote.get('id'), vote.get('audio_name')) for vote in votes]), ]
+    def get_choices(self):
+        choices = [('Стандартные голоса', [(vote.get('id'), vote.get('audio_name')) for vote in self.votes]), ]
+        return choices
+
+
+class TextConverterLoginForm(TextConverterForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def get_choices(self):
+        voice_choice = (('Стандартные голоса', [(vote.get('id'), vote.get('audio_name')) for vote in self.votes]),
+                        ('Пользовательские голоса', [(vote.get('id'), vote.get('audio_name'))
+                                                     for vote in self.user_votes]))
+        choices = [choice for choice in voice_choice if getitem(choice, 1)]
         return choices
