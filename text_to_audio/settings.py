bd50ffeff7f45d32a9ed9a5adeafa06bc71aae3b
= <jirobassik@tut.by>
2024-02-16 17:27:57 +0300

    Add config keys, fixs client auth

--- text_to_audio/settings.py
+++ text_to_audio/settings.py
@@ -13,6 +13,7 @@ import os
 from pathlib import Path
 
 from django.conf import settings
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jx5k0&^78#(qxcf-49os@zl&ryw*av(ljg1vv7c1z6ztl&d9o@'
+
+
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -53,6 +56,14 @@ INSTALLED_APPS = [
     'social_django',
     'django_extensions',
 ]
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'social_core.backends.google.GoogleOAuth2',
+]
+
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = config("SOCIAL_AUTH_GOOGLE_OAUTH2_KEY")
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = config("SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET")
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
